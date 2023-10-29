d435390c7e5e432b5d568847ad24d1eeded4a0f2
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2023-10-29 15:02:44 +0100

    -add exercises for day 1 without exercise 10

---
+++ W9_Exercises/W91_IOStreams/Book.java
@@ -0,0 +1,46 @@
+package W9_Exercises.W91_IOStreams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book  {
+    private String title;
+    private String author;
+    private int publicationYear;
+    StringBuilder sb = new StringBuilder();
+    private List<Book> bookList = new ArrayList<>();
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(sb.append(" | " + title).append(" | " + author).append(" | " + publicationYear));
+    }
+
+}
---
+++ W9_Exercises/W91_IOStreams/MainClass_1.java
@@ -0,0 +1,18 @@
+package W9_Exercises.W91_IOStreams;
+
+public class MainClass_1 {
+    public static void main(String[] args) {
+//      User user = new User();
+//      System.out.println(user.toString());
+        User user = User.getUserInfo();
+//      user.getAddress();
+//      user.readInfo();
+//      user.getUserBirthYear();
+
+        String book = user.getFavoriteBook();
+        user.favoriteColour();
+
+
+
+    }
+}
