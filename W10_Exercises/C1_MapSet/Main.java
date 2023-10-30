867b825aa1b9bcea208421a00e96311a8f4c7973
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2023-10-30 17:39:53 +0100

    -add exercises for day 1 except ex.6 and proper exception handling

--- W10_Exercises/C1_MapSet/Main.java
+++ W10_Exercises/C1_MapSet/Main.java
@@ -8,15 +8,19 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
-        Map<Integer, Employee> employees = employee.getMapEmployees();
-//        Map<Integer, Employee> filteredDepartment = employee.filterDepartment("IT");
-//        System.out.println(filteredDepartment);
-//       Map<Integer, Employee> filteredJobTitle = employee.filterJobTitle("Developer");
-//        System.out.println(filteredJobTitle);
-//        Map<Integer, Employee> filteredManagerID = employee.filterManagerId(42);
-//        System.out.println(filteredManagerID);
 
- //       employee.countEmployeesDepartment();
+        Map<Integer, Employee> employees = employee.getMapEmployees();
+
+        Map<Integer, Employee> filteredDepartment = employee.filterDepartment("IT");
+        System.out.println(filteredDepartment);
+
+        Map<Integer, Employee> filteredJobTitle = employee.filterJobTitle("Developer");
+        System.out.println(filteredJobTitle);
+
+        Map<Integer, Employee> filteredManagerID = employee.filterManagerId(42);
+        System.out.println(filteredManagerID);
+
+        employee.countEmployeesDepartment();
 
         Set<Employee> employeesSet = employee.getSetEmployees();
         System.out.println(employee.countingEmployees());
